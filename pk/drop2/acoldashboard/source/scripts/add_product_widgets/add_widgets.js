5273b8c3d7a782e0202c46fc8d7612accc592850
Prakash <prakemail@gmail.com>
2016-01-18 09:57:21 +0530

    dnd fix for typo

--- pk/drop2/acoldashboard/source/scripts/add_product_widgets/add_widgets.js
+++ pk/drop2/acoldashboard/source/scripts/add_product_widgets/add_widgets.js
@@ -3,26 +3,19 @@ app.controller("addWidgetsForproductCtrl", function($scope) {
     $scope.models = {
         selected: null,
         product_steps: [],
-        lists: {"A": [], "B": []}
+        lists: {"A": [], "B": []},
+        wlist:[]
     };
-
-    // Generate initial model
     for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.A.push({id:"A"+i, widgetId:"WA1" + i, label: "Widget A" + i});
-        $scope.models.lists.B.push({id:"B"+i, widgetId:"WB1" + i, label: "Widget B" + i});
+        $scope.models.lists.A.push({id:"A"+i, type:"widget", catId:"WA" + i, label: "Widget A" + i});
+        $scope.models.lists.B.push({id:"B"+i, type:"widget", catId:"WB" + i, label: "Widget B" + i});
+        //$scope.models.lists.steps.push({id:"step"+i, type:"step", widgetId:"WS" + i, label: "Widget S" + i});
     }
-    $scope.models.product_steps.push({id:"A1", widgetId:"WA1", label: "Widget A1"});
-
+    $scope.models.product_steps.push({id:"A1", type:"step", catId:"WA1", label: "Widget 0A1"});
+    $scope.models.product_steps.push({id:"A2", type:"step", catId:"WA2", label: "Widget 0A2"});
+    
     // Model to JSON for persist the widget config purpose
     $scope.$watch('models', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
-
-    $scope.dropCallback = function(event, index, item, external, type, allowedType) {
-        if (external) {
-            if (allowedType === 'itemType' && !item.label) return false;
-            if (allowedType === 'containerType' && !angular.isArray(item)) return false;
-        }
-        return item;
-    };
 });
